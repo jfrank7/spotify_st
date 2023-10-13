aa769fdd5288338b04a629570883602d8f633192
jfrank <67676243+jfrank7@users.noreply.github.com>
2023-10-13 14:18:41 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,11 @@
 import streamlit as st
-import os
 import json
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
-import spotipy.util as util
 import pandas as pd
-import socket
 
 
-def get_spotify(submitted, client_id_input, client_secret_input):
+def get_spotify():
     """
     Extract and transform spotify data into a dataframe with top 50 songs
     Output: Dataframe
@@ -18,74 +14,31 @@ def get_spotify(submitted, client_id_input, client_secret_input):
     # Change environment variables
     cid = '350a1fdf522145b6b70a780c4f1f3233'
     secret = 'e691637d43c04683897fec63a069d797'
-    
-    if submitted:
-        # Set environment variables with the provided credentials
-        cid = client_id_input
-        secret = client_secret_input
 
     # Create a Spotipy client
-
-    # Attempt to close the socket if it's already open
-    try:
-        existing_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        existing_socket.connect(('localhost', 7777))
-        existing_socket.close()
-        print("Closed the existing socket on localhost:7777.")
-    except ConnectionRefusedError:
-        # The connection was refused, meaning the socket wasn't open.
-        print("No existing socket found on localhost:7777.")
-    except Exception as e:
-        print(f"An error occurred while closing the socket: {e}")
-        
-        # Attempt to close the socket if it's already open
-    try:
-        existing_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        existing_socket.connect(('localhost', 8501))
-        existing_socket.close()
-        print("Closed the existing socket on localhost:8501.")
-    except ConnectionRefusedError:
-        # The connection was refused, meaning the socket wasn't open.
-        print("No existing socket found on localhost:7777.")
-    except Exception as e:
-        print(f"An error occurred while closing the socket: {e}")
-
-    # # Specify the desired host and port
-    # host = 'localhost'
-    # port = 7777 
-
-    # # Create and bind the socket
-    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # server_socket.bind((host, port))
-
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id = cid,
         client_secret = secret,
         redirect_uri = 'http://localhost:7777/callback',
         scope = 'user-top-read'
         ))
-    
-    # token = util.prompt_for_user_token(
-    #     client_id = cid,
-    #     client_secret = secret,
-    #     redirect_uri = 'http://localhost:7777/callback',
-    #     scope = 'user-top-read'
-    #     )
-    # sp = spotipy.Spotify(auth=token)
-    
+
     # Get the user's top 50 tracks
     results = sp.current_user_top_tracks(
         limit = 50,
         offset = 0,
         time_range = 'short_term' # short_term = 4 weeks, medium_term = 6 months
         )
+        
+    return results
 
+def transform_spotify(spotify_data):
     # Process the list of top tracks
     list_of_artist_names = []
     list_of_song_names = []
     list_of_albums = []
 
-    for result in results['items']:
+    for result in spotify_data['items']:
         list_of_artist_names.append(result["artists"][0]["name"])
         list_of_song_names.append(result["name"])
         list_of_albums.append(result["album"]["name"])
@@ -137,41 +90,55 @@ st.header('Spotify Top 10s')
 # Display the description
 st.subheader('Description')
 
-st.write('''Log in here to obtain your Client ID and Client Secret: https://developer.spotify.com/dashboard/login
+st.write('''This app uses the Streamlit platform as a user friendly way to interact with.
+         
+It interacts with Spotify's API to access live data on the user and display it in a simple table.
 
-Then click on your username at the top right, then on "dashboards".
+All three steps of the ETL process are followed to reach the end result.
 
-There create an app. Add any name and description, and copy this for the **Redirect URI:** http://localhost:7777/callback
-
-Finally, open the app and click on "Settings" at the top right of the screen and you will find your credentials.
-
-**Disclaimer:**
-These credentials are used to access your accounts data, not the account itself. They cannot be used to modify your account in any way. The credentials are not saved anywhere, they are used temporarily to access your data locally.
-        ''')
-
-# Display input request to get credentials
-with st.form("my_form"):
-    st.subheader('Credentials')
-
-    # Input fields for Client ID and Client Secret
-    client_id_input = st.text_input('**Client ID**', placeholder='Insert CID...')
-    client_secret_input = st.text_input('**Client Secret**', placeholder='Insert secret...')
-    submitted = st.form_submit_button("Submit")
-
-# Extract and transform spotify data
-all_songs = get_spotify(submitted, client_id_input, client_secret_input)
+Data cleaning had to be applied to the artists variable to account for songs with multiple artists. For simplicity the main artist according to spotify was used and displayed.
+''')
 
 # Display section header
 st.subheader('Your Spotify Top 10s')
 
-if not submitted:
-    st.write('This is an example...')
+# Streamlit button 1
+if st.button("Run Spotify Analysis (LOGIN REQUIRED)"):
+    # Extract and transform Spotify data
+    all_songs = transform_spotify(get_spotify())
 
-# Display the top 10 songs
-st.write('**Top 10 Songs**')
-st.dataframe(top10_songs(all_songs))
+    # Display the charts side by side
+    col1, col2 = st.columns(2)
+    
+    # Display the top 10 songs
+    with col1:
+        st.write('**Top 10 Songs**')
+        st.dataframe(top10_songs(all_songs))
 
-# Display the top 10 artists
-st.write('**Top 10 Artists**')
-st.dataframe(top10_artists(all_songs))
+    # Display the top 10 artists
+    with col2:
+        st.write('**Top 10 Artists**')
+        st.dataframe(top10_artists(all_songs))
+    
+# Streamlit button 2
+if st.button("Show example"):
+    # Extract and transform Spotify data
+    
+    # Open the JSON file for reading
+    with open('example.json', 'r') as json_file:
+        # Load the JSON data into the dictionary
+        data = json.load(json_file)
+    all_songs = transform_spotify(data)
 
+    # Display the charts side by side
+    col1, col2 = st.columns(2)
+    
+    # Display the top 10 songs
+    with col1:
+        st.write('**Top 10 Songs**')
+        st.dataframe(top10_songs(all_songs))
+
+    # Display the top 10 artists
+    with col2:
+        st.write('**Top 10 Artists**')
+        st.dataframe(top10_artists(all_songs))
